498d582be04950fd86a607d8f3389fc378d6dc23
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2021-08-20 17:42:27 +0100

    modified my info

--- resume/settings.py
+++ resume/settings.py
@@ -43,7 +43,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['myreseumee.herokuapp.com', '127.0.0.1']
 
